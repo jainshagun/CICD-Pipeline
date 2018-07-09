1cca2fdf9736cd3d13855ffa0c9b4bac217f3828
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-07-09 11:03:24 +0100

    Update RunCucumberTest.java

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"};strict = false, features = &quot;features&quot;, format = { &quot;pretty&quot;,
+        &quot;json:target/cucumber.json&quot; }, tags = { &quot;~@ignore&quot; })
 public class RunCucumberTest {
 }
